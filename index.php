2703ecf1d6a4dc1852340084eba8cc4e59df6a3b
Álvaro Silva Rodríguez <asilva@alumnos.imf.com>
2021-02-04 11:38:55 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+<head>
+    <meta name="author" content="Álvaro Silva Rodríguez">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="keywords" content="Flexbox">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/estilosFlex.css">
+    <title>Flexbox</title>
+</head>
+<body>
+<main>
+    <header>
+        <h1>PSYKOSHISHAS</h1>
+        <a href="index.html"><p>Portada</p></a>
+        <img src="img/PsichoShishas_img_Logo_rectangular2.jpg" class="logo">
+
+    </header>
+    <section>
+        <p class="eslogan">¡Deja fuera los malos humos!</p>
+    <div class="contenedores">
+        <img src="img/japona.jpg" class="imagen">
+    </div>
+    <div class="contenedores">
+        <img src="img/steamulation.jpg" class="imagen">
+    </div>
+    <div class="contenedores">
+        <img src="img/vz.jpg" class="imagen">
+    </div>
+    <div class="contenedores">
+        <img src="img/alpha.jpg" class="imagen">
+    </div>
+    </section>
+</main>
+</body>
+</html>
